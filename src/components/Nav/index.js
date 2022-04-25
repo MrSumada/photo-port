da8f8ffa8f4b36684e2cd55105a20f589974ace4
MrSumada <ricksamericana.com>
2022-04-25 13:42:33 -0400

    add about and nav components

---
+++ src/components/Nav/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
+
+
+function Nav() {
+
+    const categories = [
+        {
+            name: "commercial",
+            description: "Photos of grocery stores, food trucks, and other commercial projects",
+        },
+        { name: "portraits", description: "Portraits of people in my life" },
+        { name: "food", description: "Delicious delicacies" },
+        {
+            name: "landscape",
+            description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+        },
+    ];
+
+    function categorySelected(name) {
+        console.log(`${name} is nice`);
+    }
+
+    return(
+        <header className="my-5">
+            <h2>
+                <a href="/"><span role="img" aria-label="camera"> 📸</span> Oh Snap!</a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <a href="#about">About Me</a>
+                    </li>
+                    <li className="mx-1">
+                        <a href="#gallery">gallery</a>
+                    </li>
+                    <li className="mx-1">
+                        <span>contact</span>
+                        {/* <a href="#contact">Contact</a> */}
+                    </li>
+                    {categories.map((category) => (
+                        <li className="mx-1" key={category.name}>
+                            <span onClick={() => categorySelected(category.name)}>{capitalizeFirstLetter(category.name)}</span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Nav;
